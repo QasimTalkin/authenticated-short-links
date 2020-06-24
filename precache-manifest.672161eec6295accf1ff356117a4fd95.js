8b03ace8ff2184478928cc65922b918c461ee810
Qasimtalkin <abulqasim110@gmail.com>
2020-06-23 21:37:59 -0400

    Updates

---
+++ precache-manifest.672161eec6295accf1ff356117a4fd95.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "537f2728a9ba2cf305e3622223c80f13",
+    "url": "/authenticated-short-links/index.html"
+  },
+  {
+    "revision": "061e66f47a1a7a8273fd",
+    "url": "/authenticated-short-links/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "174345e9d58cf38a2697",
+    "url": "/authenticated-short-links/static/js/2.83126d97.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/authenticated-short-links/static/js/2.83126d97.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "061e66f47a1a7a8273fd",
+    "url": "/authenticated-short-links/static/js/main.fdcb7cf2.chunk.js"
+  },
+  {
+    "revision": "352b3eda688231b072ef",
+    "url": "/authenticated-short-links/static/js/runtime-main.416b3f76.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/authenticated-short-links/static/media/logo.5d5d9eef.svg"
+  }
+]);
